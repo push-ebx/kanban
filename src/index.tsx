5dfd37f531e2ae5186c3c98702de95357ec6e73a
push-ebx <nikita.keyn@yandex.ru>
2023-10-14 21:34:39 +0300

    update README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './app'
+import bridge from "@vkontakte/vk-bridge";
 
+bridge.send("VKWebAppInit");
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
 )
